00eeff4c838544fca64d11fb988df1d55c432e80
ambroseang <ambrose.tk.ang@gmail.com>
2023-02-25 12:35:31 +0800

    set bg color and routing to view claim details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import ClaimsDisplayPage from './components/claims-display-page/ClaimsDisplayPage';
+import ViewClaim from './components/claim-details/ViewClaim';
 
 // import libraries
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalStyles } from "@mui/material";
 
 function App() {
   return (
     <div>
+      <GlobalStyles
+        styles={{
+          body: { backgroundColor: "#f2f0f0" },
+        }}
+      />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ClaimsDisplayPage />} />
+          <Route path="/view-claim" element={<ViewClaim />} />
         </Routes>
       </BrowserRouter>
     </div>
